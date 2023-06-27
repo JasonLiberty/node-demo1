7090fb3bdde9d8138eb790c2b62ff113c663e243
Bigger <jason.bigger@libertymutual.com>
2023-06-27 08:34:30 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// let message = "Hello World";
+// console.log(message);
+
+// Load lodash using require() as assign it to a variable named lodash (easy to remember, right?)
+let lodash = require("lodash");
+
+let name = "brittany";
+console.log("Name: " + name);
+
+// call the upperFirst() function in lodash to convert the first letter of the string to uppercase
+name = lodash.upperFirst(name);
+console.log("Revised name: " + name);
